e58491443e100b0335fe5f0c0617f2b1b3577397
andrzej1616 <119822635+andrzej1616@users.noreply.github.com>
2023-12-05 01:29:36 +0100

    testowe wprowadzenie logowania,rej

--- chess/views.py
+++ chess/views.py
@@ -11,3 +11,22 @@ def game(request, id):
 def lobby(request):
     lobbies = get_lobbies_data()
     return render(request, 'lobby/lobby.html', {'lobbies': lobbies})
+
+
+def login(request):
+    if request.method == 'POST':
+        # Tutaj umieść logikę autentykacji, sprawdzanie danych logowania itp.
+        # Jeżeli autentykacja powiedzie się, możesz przekierować użytkownika gdzieś indziej.
+        # W przeciwnym razie możesz zwrócić błąd lub informację o nieudanej próbie logowania.
+        pass
+
+    return render(request, 'login/login.html', {})
+
+def register(request):
+    if request.method == 'POST':
+        # Tutaj umieść logikę autentykacji, sprawdzanie danych logowania itp.
+        # Jeżeli autentykacja powiedzie się, możesz przekierować użytkownika gdzieś indziej.
+        # W przeciwnym razie możesz zwrócić błąd lub informację o nieudanej próbie logowania.
+        pass
+    
+    return render(request, 'register/register.html', {})
